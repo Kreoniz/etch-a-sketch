9420bc4712cf33ca2556350e87a6281a99f185e3
Kreoniz <kreoniz@mail.ru>
2022-12-15 12:43:25 +0600

    Create a grid using JavaScript

---
+++ script.js
@@ -0,0 +1,13 @@
+const canvas = document.querySelector('#canvas');
+
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+
+    for (let j = 0; j < 16; j++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        row.appendChild(cell);
+    }
+    canvas.appendChild(row);
+}
