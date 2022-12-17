fb60a95555667856cbc8a00b2ef968c0ad035bad
Kreoniz <kreoniz@mail.ru>
2022-12-17 16:05:28 +0600

    Add button to create a grid with custom width

--- script.js
+++ script.js
@@ -31,3 +31,21 @@ function fill(event) {
 }
 
 createGrid(16);
+
+const newGridButton = document.querySelector('#newGrid');
+
+newGridButton.addEventListener('click', () => {
+    let gridWidth = Number(prompt("Enter new grid's width:", 16));
+
+    while (!gridWidth || gridWidth > 100 || gridWidth <= 0) {
+        if (!Number.isInteger(gridWidth)) {
+            gridWidth = Number(prompt('Enter an integer:', 16));
+        } else if (gridWidth > 100) {
+            gridWidth = Number(prompt("Grid's width should be smaller than 100: ", 16));
+        } else if (gridWidth <= 0) {
+            gridWidth = Number(prompt("Grid's width should be greater than 0: ", 16));
+        }
+    }
+
+    createGrid(gridWidth);
+});
