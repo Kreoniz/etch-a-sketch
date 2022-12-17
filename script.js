6fcddc1006800069529ede0b8c2b2dd5955683eb
Kreoniz <kreoniz@mail.ru>
2022-12-17 15:40:09 +0600

    Make grid autoreset before creating new grid

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-const canvas = document.querySelector('#canvas');
-
 const canvasWidth = 500;
 
 function createGrid(gridWidth) {
+    const canvas = document.querySelector('#canvas');
+    canvas.innerHTML = "";
+
     for (let i = 0; i < gridWidth; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
 
         for (let j = 0; j < gridWidth; j++) {
             const cell = document.createElement('div');
+            const cellWidth = canvasWidth / gridWidth;
             cell.classList.add('cell');
-            cell.style.width = `${canvasWidth / gridWidth}px`;
-            cell.style.height = `${canvasWidth / gridWidth}px`;
+            cell.style.width = `${cellWidth}px`;
+            cell.style.height = `${cellWidth}px`;
             row.appendChild(cell);
         }
         canvas.appendChild(row);
