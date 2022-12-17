7e3d39ede9b1d3f28794fa405c07c7eb9662c653
Kreoniz <kreoniz@mail.ru>
2022-12-17 15:29:17 +0600

    Turn grid creating into a function
    Now to create a grid with N cells you just need to pass an argument into a createGrid function

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 const canvas = document.querySelector('#canvas');
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
+const canvasWidth = 500;
 
-    for (let j = 0; j < 16; j++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        row.appendChild(cell);
+function createGrid(gridWidth) {
+    for (let i = 0; i < gridWidth; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+
+        for (let j = 0; j < gridWidth; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.style.width = `${canvasWidth / gridWidth}px`;
+            cell.style.height = `${canvasWidth / gridWidth}px`;
+            row.appendChild(cell);
+        }
+        canvas.appendChild(row);
     }
-    canvas.appendChild(row);
-}
 
-const cells = document.querySelectorAll('.cell');
-cells.forEach(cell => {
-    cell.addEventListener('mouseover', fill);
-});
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.addEventListener('mouseover', fill);
+    });
+}
 
 function fill(event) {
     const element = event.target;
     element.style.backgroundColor = 'red';
 }
+
+createGrid(16);
